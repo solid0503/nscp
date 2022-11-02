a75fbfa4c49637fb87001d9b9830d4ae3b265500
solid0503 <solid0503@gmail.com>
2022-11-02 10:53:48 +0900

    bepsim 추가

---
+++ bepsim/src/main/java/com/uangel/ktiscp/nscp/bepsim/BepsimApplication.java
@@ -0,0 +1,13 @@
+package com.uangel.ktiscp.nscp.bepsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BepsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BepsimApplication.class, args);
+	}
+
+}
---
+++ bepsim/src/test/java/com/uangel/ktiscp/nscp/bepsim/BepsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uangel.ktiscp.nscp.bepsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BepsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
